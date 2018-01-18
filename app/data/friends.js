30b0faf3784cb7ff81c688efe840681934f03650
Nadir <Jaffan_Nadir@hotmail.com>
2018-01-17 18:41:20 -0800

    first commit

---
+++ app/data/friends.js
@@ -0,0 +1,48 @@
+var friends = [
+  {
+    name: "Nadir",
+    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAwBAAAAJGNiOWMyM2UwLTNjZjktNDg2YS04NDcyLTYyY2NhMThhODMzMQ.jpg",
+    scores: [
+      "5",
+      "1",
+      "4",
+      "4"
+      
+    ]
+  },
+  {
+    name: "Salma",
+    photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7b/Salma_Hayek_2%2C_2012.jpg/440px-Salma_Hayek_2%2C_2012.jpg",
+    scores: [
+      "4",
+      "2",
+      "5",
+      "2"
+    ]
+  },
+  {
+    name: "Catherine Zeta",
+    photo: "https://upload.wikimedia.org/wikipedia/commons/d/df/Catherine_Zeta-Jones_VF_2012_Shankbone_2.jpg",
+    scores: [
+      "5",
+      "2",
+      "2",
+      "5"
+    ]
+  },
+  {
+    name: "Pam",
+    photo: "http://i.dailymail.co.uk/i/pix/2016/04/22/05/336C186300000578-0-image-a-2_1461299617790.jpg",
+    scores: [
+      "3",
+      "3",
+      "4",
+      "3"
+    ]
+  }
+ 
+  
+
+];
+
+module.exports = friends;
